df71438d864f375cfd725d9e8963a352e476224a
Luke <2342915h@student.gla.ac.uk>
2022-09-16 17:53:49 +0100

    Optimised the mcu_movies script to store less data

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from ctypes import cast
 import os
 import json
 import requests
@@ -14,13 +15,38 @@ data = req.json()
 movies_from_list = data.get('items', [])
 
 # loop through and get a dictionary of credits - movie_id: credits
-credits_movie = {}
-for movie in movies_from_list[:5]:
+people = {}
+for movie in movies_from_list:
     req = requests.get(f'{URL}movie/{movie["id"]}/credits?api_key={KEY}')
     data = req.json()
-    movie['cast'] = data.get('cast', [])
-    movie['crew'] = data.get('crew', [])
+    # store all the cast
+    movie['cast'] = []
+    for cast_member in data.get('cast', []):
+        movie['cast'].append({
+            'id': cast_member.get('id'),
+            'character': cast_member.get('character', ''),
+        })
+        people[cast_member.get('id')] = {
+            'name': cast_member.get('name', ''),
+            'known_for': cast_member.get('known_for_department', ''),
+            'profile_path': cast_member.get('profile_path', '')
+        }
+    # store all the crew
+    movie['crew'] = []
+    for crew_member in data.get('crew', []):
+        movie['crew'].append({
+            'id': crew_member.get('id'),
+            'job': crew_member.get('job', ''),
+        })
+        people[crew_member.get('id')] = {
+            'name': crew_member.get('name', ''),
+            'known_for': crew_member.get('known_for_department', ''),
+            'profile_path': crew_member.get('profile_path', '')
+        }
 
 # store dictionary with information as a json file
 with open('mcu_movies.json', 'w') as file:
-    json.dump(movies_from_list, file, indent=2)
+    json.dump({
+        'movies': movies_from_list,
+        'people': people,
+    }, file, indent=2)
